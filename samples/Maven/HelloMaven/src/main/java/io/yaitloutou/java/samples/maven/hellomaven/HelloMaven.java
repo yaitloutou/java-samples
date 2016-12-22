46216d9ca91a94e986281db230e4505c483e11bf
yaitloutou <yaitloutou@gmail.com>
2016-12-22 00:00:17 +0000

    create a maven project

---
+++ samples/Maven/HelloMaven/src/main/java/io/yaitloutou/java/samples/maven/hellomaven/HelloMaven.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.yaitloutou.java.samples.maven.hellomaven;
+
+/**
+ *
+ * @author yaitloutou
+ */
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
